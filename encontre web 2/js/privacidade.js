16f01e6b567c6239ac0013cc6a7000b4eeefea9e
gui24123e3 <guilhermelopesrocetom124567@gmail.com>
2025-03-02 16:37:30 -0300

    Delete encontre web 2 directory

--- encontre web 2/js/privacidade.js
+++
@@ -1,6 +0,0 @@
-function showTranslation(lang) {
-    document.querySelectorAll('.content').forEach((el) => {
-        el.style.display = 'none';
-    });
-    document.querySelector('#content-' + lang).style.display = 'block';
-}
